8e4629a5a6ab559907a992ebb994b1a3d3870910
Vansh Bhargava <vansh1501.be21@chitkara.edu.in>
2024-01-01 09:40:25 +0530

    updated links

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -24,7 +24,7 @@ Consulting a healthcare professional, buying medicine, ordering lab tests and im
                 </p>
                 <div className="flex flex-col items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
                   <Link
-                    href="https://nextjstemplates.com/templates/saas-starter-startup"
+                    href="https://www.linkedin.com/in/vansh-bhargava-304036219/"
                     className="rounded-sm bg-primary px-8 py-4 text-base font-semibold text-white duration-300 ease-in-out hover:bg-primary/80"
                   >
                     Get Free Consolation

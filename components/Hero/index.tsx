c2c1006260fa18e6dcbb15edd72aa4e933aadf20
Vansh Bhargava <vansh1501.be21@chitkara.edu.in>
2024-01-01 09:30:54 +0530

    updated the link

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -30,7 +30,7 @@ Consulting a healthcare professional, buying medicine, ordering lab tests and im
                     Get Free Consolation
                   </Link>
                   <Link
-                    href="https://github.com/NextJSTemplates/startup-nextjs"
+                    href="https://github.com/VanshBhargava/hospital-management"
                     className="inline-block rounded-sm bg-black px-8 py-4 text-base font-semibold text-white duration-300 ease-in-out hover:bg-black/90 dark:bg-white/10 dark:text-white dark:hover:bg-white/5"
                   >
                     Book an appointment
